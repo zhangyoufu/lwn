019e6bce5421ecd3d5c6e1832b656a2b4b5bf780
张酉夫 <zhangyoufu@gmail.com>
2019-11-27 10:56:21 +0800

    suppress pushd/popd output

--- .github/cron.sh
+++ .github/cron.sh
@@ -13,13 +13,13 @@ git config --global user.email "$(whoami)@$(hostname --fqdn)"
 git config --global http.https://github.com/.extraheader "Authorization: Basic $(echo -n "dummy:${GITHUB_PERSONAL_ACCESS_TOKEN}" | base64 --wrap=0)"
 
 push_if_modified() {
-	pushd "$1"
+	pushd "$1" 2>/dev/null
 	if [ -n "$(git status --porcelain)" ]; then
 		git add --all
 		git commit --amend --reset-author --message 'automatic commit'
 		git push --force
 	fi
-	popd
+	popd 2>/dev/null
 }
 
 push_if_modified gh-pages
