a966807ba8ffed0af1b9c299d5dfefb7ca8d354c
张酉夫 <zhangyoufu@gmail.com>
2020-02-08 13:18:00 +0800

    refine CI

--- .github/commit.sh
+++ .github/commit.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 set -ex
 
-pip3 install -r .github/requirements.txt
-
-git worktree add -b gh-pages gh-pages origin/gh-pages
-git worktree add -b state state origin/state
-
-.github/cron.py
-
 git config user.name 'GitHub Actions'
 git config user.email "$(whoami)@$(hostname --fqdn)"
 git config http.https://github.com/.extraheader "Authorization: Basic $(echo -n "dummy:${GITHUB_PERSONAL_ACCESS_TOKEN}" | base64 --wrap=0)"
 
 push_if_modified() {
-	pushd "$1" 2>/dev/null
+	pushd "$1" >/dev/null
 	if [ -n "$(git status --porcelain)" ]; then
 		git add --all
 		git commit --amend --reset-author --message 'automatic commit'
 		git push --force
 	fi
-	popd 2>/dev/null
+	popd >/dev/null
 }
 
 push_if_modified gh-pages
