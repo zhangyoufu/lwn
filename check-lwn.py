854ea683c8e0d963a686240f41b0a5edfae4cb2c
Job <jobse@pm.me>
2022-06-14 21:53:09 +0200

    Add missing dc prefix

--- check-lwn.py
+++ check-lwn.py
@@ -112,6 +112,7 @@ rss = f'''\
   xmlns="http://purl.org/rss/1.0/"
   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
   xmlns:syn="http://purl.org/rss/1.0/modules/syndication/"
+  xmlns:dc="http://purl.org/dc/elements/1.1/"
   xmlns:atom="http://www.w3.org/2005/Atom"
 >
   <channel rdf:about="{os.environ['FEED_URL']}">
